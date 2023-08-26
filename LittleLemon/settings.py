8ef3f623d31372717834f34278314eef48593e04
Shahmil <64994036+shahmilc@users.noreply.github.com>
2023-08-26 11:09:14 -0400

    Remove XML renderer

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -129,7 +129,6 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_xml.renderers.XMLRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',

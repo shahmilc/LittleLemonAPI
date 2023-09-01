6ac6e1823ebdc2d23928c805d140cfa455c79304
Shahmil <64994036+shahmilc@users.noreply.github.com>
2023-09-01 12:21:57 -0400

    Setup Djoser auth layer

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
     path('api/', include('LittleLemonAPI.urls')),
 ]
